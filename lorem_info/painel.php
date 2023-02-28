d6e245f74746b361271c096dff3248d9d71d21c7
Felipe <120607893+f95f@users.noreply.github.com>
2023-02-28 02:19:23 -0300

    added wireframes, color pallete and basic painel structure

--- lorem_info/painel.php
+++ lorem_info/painel.php
@@ -2,17 +2,33 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel | Lorem Info</title>
     <link rel="stylesheet" href="styles/style_main.css">
 </head>
 <body>
-    <head>
-        <h1>Olá, <?php #usuário?></h1>
-    </head>
-    <main>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab dolorem dicta, odio, itaque ducimus quia voluptas repellendus quisquam eligendi neque nihil perspiciatis vel illo amet ratione, qui delectus voluptatem nulla!</p>
+    <header>
+        <div id="logomarca">
+            <img src="" alt="" id = "logo">
+            <h1>Painel de  Serviços</h1>
+        </div>
+
+        <div id = "user_greet">
+            <h2>Olá, <?php #usuário?></h2>    
+        </div>
+        
+    </header>
+    <main>        
+        <div class = "card">1</div>
+        <div class = "card">2</div>
+        <div class = "card">3</div>
+        <div class = "card">4</div>
+        <div class = "card">5</div>
+        <div class = "card">6</div>
+        <div class = "card">7</div>
+        <div class = "card">8</div>
     </main>
     <footer>
         <span>95C @ IFSP Guarulhos</span>
