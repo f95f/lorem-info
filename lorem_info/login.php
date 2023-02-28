b298433f16ed572fedb16df434b180bfc893ba01
Felipe <120607893+f95f@users.noreply.github.com>
2023-02-28 00:05:01 -0300

    initial files setup

---
+++ lorem_info/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorem Info | Identifique-se</title>
+    <link rel="stylesheet" href="styles/style_main.css">
+
+</head>
+<body>
+    <main>
+        <h1>Olá!</h1>
+        <h2>Identifique-se:</h2>
+        <form action="" method = "post">
+        </form>
+    </main>
+</body>
+</html>
---
+++ lorem_info/painel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel | Lorem Info</title>
+    <link rel="stylesheet" href="styles/style_main.css">
+</head>
+<body>
+    <head>
+        <h1>Olá, <?php #usuário?></h1>
+    </head>
+    <main>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab dolorem dicta, odio, itaque ducimus quia voluptas repellendus quisquam eligendi neque nihil perspiciatis vel illo amet ratione, qui delectus voluptatem nulla!</p>
+    </main>
+    <footer>
+        <span>95C @ IFSP Guarulhos</span>
+    </footer>
+</body>
+</html>
