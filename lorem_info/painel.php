af37ea48da6f42363c1a81089bfee240399b3439
Felipe <120607893+f95f@users.noreply.github.com>
2023-03-03 16:07:50 -0300

    modified content, styling

--- lorem_info/painel.php
+++ lorem_info/painel.php
@@ -7,11 +7,23 @@
 
 <main>        
     <?php
-    
+
+        $cardInfo = [
+            ['img' => '',
+             'title' => 'simular orçamento', 
+             'desc' => 'Simula uma ordem de serviço antes de criá-la.']
+            ];
+
         for($i = 0; $i < 8; $i++){
-            echo '<div class = "card" id = "action-'.$i.'">'. $i + 1 .'</div>';
+            echo '<div class = "card" id = "action-'.$i.'">';
+            #if($i == 0){
+                echo '<img class = "icons" src = "'.$cardInfo[0]['img'].'">';
+                echo '<h3>'.$cardInfo[0]['title'].'</h3>';
+                echo '<p>'.$cardInfo[0]['desc'].'</p>';
+            #}
+            echo '</div>';
         }
-    
+        
     ?>
 </main>
 
