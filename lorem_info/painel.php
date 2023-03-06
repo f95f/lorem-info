021679b744229072404f25e8dfdac49a38184a36
Felipe <120607893+f95f@users.noreply.github.com>
2023-03-05 22:58:45 -0300

    added some icons

--- lorem_info/painel.php
+++ lorem_info/painel.php
@@ -8,19 +8,57 @@
 <main>        
     <?php
 
-        $cardInfo = [
-            ['img' => '',
-             'title' => 'simular orçamento', 
-             'desc' => 'Simula uma ordem de serviço antes de criá-la.']
-            ];
+        $cardInfo = 
+        [
+            [
+                'img' => 'icon_orçamento.png',
+                'title' => 'Simular Orçamento', 
+                'desc' => 'Simula uma ordem de serviço antes de criá-la.'
+            ],
+            [
+                'img' => 'icon_criarOS.png',
+                'title' => 'Criar OS', 
+                'desc' => 'Cria uma nova ordem de serviço.'
+            ],
+            [
+                'img' => 'icon_consultarOS.png',
+                'title' => 'Consultar OS', 
+                'desc' => 'Pesquisar, alterar ou excluir ordens de serviço.'
+            ],
+            [
+                'img' => 'icon_cadastrarServ.png',
+                'title' => 'Cadastrar Serviços', 
+                'desc' => 'Cadastra um novo serviço.'
+            ],
+            [
+                'img' => 'icon_consultarServ.png',
+                'title' => 'Consultar Serviços', 
+                'desc' => 'Pesquisar, alterar ou excluir serviço cadastrados.'
+            ],
+            [
+                'img' => 'icon_cadastrarLogin.png',
+                'title' => 'Cadastrar Login', 
+                'desc' => 'Cadastrar um novo acesso ao sistema.'
+            ],
+            [
+                'img' => 'icon_alterarLogin.png',
+                'title' => 'Alterar Login', 
+                'desc' => 'Alterar acesso de funcionários.'
+            ],
+            [
+                'img' => 'icon_sobre.png',
+                'title' => 'Sobre', 
+                'desc' => 'Mais informações sobre o sistema.'
+            ],
+        ];
 
         for($i = 0; $i < 8; $i++){
             echo '<div class = "card" id = "action-'.$i.'">';
-            #if($i == 0){
-                echo '<img class = "icons" src = "'.$cardInfo[0]['img'].'">';
-                echo '<h3>'.$cardInfo[0]['title'].'</h3>';
-                echo '<p>'.$cardInfo[0]['desc'].'</p>';
-            #}
+            
+            echo '<img class = "icons" src = "images/'.$cardInfo[$i]['img'].'">';
+            echo '<h3>'.$cardInfo[$i]['title'].'</h3>';
+            echo '<p>'.$cardInfo[$i]['desc'].'</p>';
+
             echo '</div>';
         }
         
